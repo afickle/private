42c59ee281961edc7b250caa9214bfb0583b7710
ayfickle <1365818447@qq.com>
2017-03-21 15:49:03 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -11,54 +11,53 @@ var mime = require('mime');
 
 var cache = {};
 
-function send404(response){
-    response.writeHead(404,{'Content-Type': 'Text/plain'});
+function send404(response) {
+    response.writeHead(404, { 'Content-Type': 'Text/plain' });
     response.write('Error 404: resource not found.');
     response.end();
 }
 
-function sendFile(response,filePath,fileContents){
+function sendFile(response, filePath, fileContents) {
     response.writeHead(
-        200,
-        {"content-type":mime.lookup(path.basename(filePath))}
+        200, { "content-type": mime.lookup(path.basename(filePath)) }
     );
     response.end(fileContents);
 }
 
-function serverStatic(response,cache,absPath){
+function serverStatic(response, cache, absPath) {
     if (cache[absPath]) {
-        sendFile(response,absPath,cache[absPath]);
-    }else {
-        fs.exists(absPath,function(exists){
-            if(exists){
-                fs.readFile(absPath, function (err,data) {
-                    if(err){
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
                         send404(response);
-                    }else{
+                    } else {
                         cache[absPath] = data;
-                        sendFile(response,absPath,data);
+                        sendFile(response, absPath, data);
                     }
                 });
-            }else{
+            } else {
                 send404(response);
             }
         });
     }
 }
 
-var server = http.createServer(function(request , response) {
+var server = http.createServer(function(request, response) {
     var filePath = false;
 
-    if (request.url == '/'){
+    if (request.url == '/') {
         filePath = 'views/index.html';
-    }else{
-        filePath =  request.url;
+    } else {
+        filePath = request.url;
     }
 
     var absPath = './' + filePath;
-    serverStatic(response,cache,absPath);
+    serverStatic(response, cache, absPath);
 });
 
- server.listen(8088,function() {
-     console.log("Server listen on port 8088.");
- });
+server.listen(8088, function() {
+    console.log("Server listen on port 8088.");
+});
