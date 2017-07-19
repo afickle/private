11b1c0bc6b26b7eed8858ffe348d426b1eb28a89
fickle <1365818447@qq.com>
2017-07-19 14:56:17 +0800

    边距叠加

--- app/scripts/commTest/reg.js
+++ app/scripts/commTest/reg.js
@@ -3,3 +3,11 @@ var regChinese = /[\u4e00-\u9fa5]/;
 console.log(regChinese.test('wo'));
 console.log(regChinese.test('我'));
 
+var fibonacci = function(n) {
+    let array = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        let index = i % 2;
+        array[index] = array[0] + array[1];
+    }
+    return array[n % 2];
+}
